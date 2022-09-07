296f6f6957325ab87b89fa8a28b5d9c2c0c73eb9
Jon-Cerr <alancomando@gmail.com>
2022-09-06 20:07:44 -0500

    Subida del paquete

---
+++ js/app.js
@@ -0,0 +1,80 @@
+const nav = document.querySelector(".nav");
+const menu = document.getElementById("enlaces");
+const menu_btn = document.getElementById("open");
+const buttons = document.querySelectorAll("btn-header");
+let closed = true;
+const menuLinks = document.querySelectorAll('#enlaces a[href^="#"]');
+
+window.addEventListener("scroll", function () {
+  nav.classList.toggle("active", window.scrollY > 0);
+});
+
+new TypeIt("#terror__span__typeit", {
+  speed: 50,
+  startDelay: 900,
+})
+  .type("Lo mejor en audio e instrumentos. ", { delay: 100 })
+  .pause(300)
+  .type(
+    `Solo en <strong style="color: #508c8b; font-family: 'Courier New', Courier, monospace">Terror Music</strong>`,
+    { delay: 200 }
+  )
+  .go();
+
+let productos = $("#productos").offset().top,
+  sucursales = $("#sucursales").offset().top,
+  about = $("#about").offset().top;
+
+window.addEventListener("resize", () => {
+  let productos = $("#productos").offset().top,
+    sucursales = $("#sucursales").offset().top,
+    about = $("#about").offset().top;
+});
+
+$("#link__inicio").on("click", (e) => {
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    100
+  );
+});
+
+$("#link__productos").on("click", (e) => {
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: productos - 80,
+    },
+    100
+  );
+});
+
+$("#link__sucursales").on("click", (e) => {
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: sucursales + 150,
+    },
+    100
+  );
+});
+
+$("#link__about").on("click", (e) => {
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: about,
+    },
+    100
+  );
+});
+
+window.addEventListener("resize", () => {
+  if (screen.width >= 700) {
+    closed = true;
+    menu.style.removeProperty("overflow");
+    menu.style.removeProperty("width");
+  }
+});
